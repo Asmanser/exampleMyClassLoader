f601af34512358da92b754019c3e3dc92646b509
Alexander Kazimov <bybirmybir@gmail.com>
2019-01-23 14:32:17 +0300

    I updated Main class

--- MyClassLoader/src/by/andersen/training/Main.java
+++ MyClassLoader/src/by/andersen/training/Main.java
@@ -4,20 +4,15 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
 
-        MyClassLoader myClassLoader1 = new MyClassLoader(
-"D:\\WORK\\Andersen\\MyClass.class");
-        myClassLoader1.cashLoad("by.andersen.training.MyClass");
-        Class<?> aClass1 = myClassLoader1.loadClass("by.andersen.training.MyClass");
-        MyInterface o = (MyInterface) aClass1.newInstance();
-        o.print();
-        MyClassLoader myClassLoader2 = new MyClassLoader("" +
-                "D:\\WORK\\Andersen\\exampleAlgorithms\\Algorithms\\out\\production\\Algorithms\\by\\andersen\\training\\QuickSort\\QuickSort.class");
-        myClassLoader2.cashLoad("by.andersen.training.QuickSort.QuickSort");
-        Class<?> aClass2 = myClassLoader2.loadClass("by.andersen.training.QuickSort.QuickSort");
-        Object o1 = aClass2.newInstance();
-
-        Class<?> bClass = myClassLoader2.loadClass("by.andersen.training.QuickSort.QuickSort");
-        Object o2 = bClass.newInstance();
+        MyClassLoader myClassLoader = new MyClassLoader(
+"D:\\WORK\\Andersen\\exampleMyClassLoader\\MyClass.class");
+        myClassLoader.cashLoad("by.andersen.training.MyClass");
+        Class<?> aClass = myClassLoader.loadClass("by.andersen.training.MyClass");
+        MyInterface o1 = (MyInterface) aClass.newInstance();
+        o1.print();
+        Class<?> bClass = myClassLoader.loadClass("by.andersen.training.MyClass");
+        MyInterface o2 = (MyInterface) bClass.newInstance();
+        o2.print();
     }
 
 }
